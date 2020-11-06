67d2c1f2c679c48cdc16ae2d1d91c5f5cd41c9fc
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-11-06 15:40:11 +0100

    Exercise useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from 'react'
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
-};
-
-export default ErrorExample;
+  let title = 'random title'
+  const handleClick = () => {
+    title = 'hello leute'
+    console.log(title)
+  }
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  )
+}
+export default ErrorExample
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'hello from here',
+  })
 
-export default UseStateObject;
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState('24')
+  const [message, setMessage] = useState('hello from here')
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: 'hellooo!!!' })
+    setMessage('hello my friend')
+  }
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  )
+}
+
+export default UseStateObject
